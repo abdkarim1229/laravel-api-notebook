ca861365b3ccd63268c5f7b789bba44719cee66d
abdkarim1229 <abdulkarimklj@gmail.com>
2021-09-27 21:44:36 +0700

    add-table-users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::get(['id','image', 'name', 'email', 'gender']);
+        return $this->successMessage(200, 'success', $users);
+    }
+    public function store(UserRequest $request)
+    {
+        $request->validate([
+            'email' => 'required|unique:users,email'
+        ]);
+        $register = $request->all();
+        $gambar = $request->file('image')->store('public/image/user');
+        $register['image'] = Storage::url($gambar);
+        $user = User::create($register);
+        return $this->successMessage(201, 'created', $user);
+    }
+
+    public function show($id)
+    {
+        $user = User::where('id', $id)->first();
+        if (!$user) {
+            return $this->errorMessage(404, 'Not Found');
+        }
+        return $this->successMessage(200, 'success', $user);
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::where('id', $id)->first();
+        if (!$user) {
+            return $this->errorMessage(404, 'Not Found');
+        }
+        $request->validate([
+            'email' => 'required|unique:users,email,' . $user->id
+        ]);
+        $data = $request->all();
+        if ($request->image != null) {
+            @unlink(public_path($user->image));
+            $gambar = $request->file('image')->store('public/image/user');
+            $data['image'] = Storage::url($gambar);
+        }
+        $user->update($data);
+        return $this->successMessage(200, 'success', $data);
+    }
+    public function destroy($id)
+    {
+        $user = User::where('id', $id)->first();
+        if (!$user) {
+            return $this->errorMessage(404, 'Not Found');
+        }
+        if ($user->image != null) {
+            @unlink(public_path($user->image));
+        }
+        $user->delete();
+    }
+
+    // Return All Message Condition
+    function errorMessage($status, $message)
+    {
+        return response([
+            'status' => $status,
+            'message' => $message
+        ]);
+    }
+    function successMessage($status, $message, $data = [])
+    {
+        return response([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\NotebookController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('notebooks', NotebookController::class);
+Route::apiResource('users', UserController::class);
